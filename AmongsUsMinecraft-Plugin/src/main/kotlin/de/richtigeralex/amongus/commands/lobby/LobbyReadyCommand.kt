2ba0cc94ebfe1d73f47861d46ad4f4de1f982c5c
RichtigerAlex <61325844+RichtigerAlex@users.noreply.github.com>
2021-08-19 10:09:02 +0200

    Update LobbyReadyCommand.kt

--- AmongsUsMinecraft-Plugin/src/main/kotlin/de/richtigeralex/amongus/commands/lobby/LobbyReadyCommand.kt
+++ AmongsUsMinecraft-Plugin/src/main/kotlin/de/richtigeralex/amongus/commands/lobby/LobbyReadyCommand.kt
@@ -46,7 +46,11 @@ class LobbyReadyCommand(private val amongUsPlayerManager: AmongUsPlayerManager, 
                     )
                 )
             }
-
+            
+            /**
+                whole thing has to reworked
+                very stupid way coded
+            */
             Bukkit.getScheduler().scheduleSyncRepeatingTask(AmongUs.instance, Runnable {
                 Bukkit.broadcastMessage("§e$counter")
                 counter--
